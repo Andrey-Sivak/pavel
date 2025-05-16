c60539c85be5f5663f00ade103c501172e22c84d
Andrey-Sivak <andrey.sivak.dev@gmail.com>
2025-05-17 02:32:34 +0300

    hot fix

---
+++ guten/src/utils/InspectorPanelBlockId.js
@@ -0,0 +1,24 @@
+import { InspectorControls } from '@wordpress/block-editor';
+import { PanelBody, TextControl } from '@wordpress/components';
+
+const InspectorPanelBlockId = ({ blockId, setAttributes }) => {
+	return (
+		<InspectorControls>
+			<PanelBody title="Block ID">
+				<TextControl
+					value={blockId}
+					onChange={(newId) => {
+						const sanitizedId = newId
+							.toLowerCase()
+							.replace(/\s+/g, '-')
+							.replace(/[^a-z0-9-]/g, '');
+						setAttributes({ blockId: sanitizedId });
+					}}
+					help="Enter a unique ID for this section (e.g. 'support-section-1'). Use only lowercase letters, numbers and hyphens"
+				/>
+			</PanelBody>
+		</InspectorControls>
+	);
+};
+
+export default InspectorPanelBlockId;
