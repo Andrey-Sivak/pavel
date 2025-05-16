c04bbb5aac7dd785f55af99e81205636db204b64
Andrey-Sivak <andrey.sivak.dev@gmail.com>
2025-05-16 23:18:22 +0300

    refactor(build): set path configuration for consistent formatting

--- gulp/config/path.js
+++ gulp/config/path.js
@@ -1,33 +1,33 @@
 import * as nodePath from 'path';
 
-const rootFolder = nodePath.basename( nodePath.resolve() );
+const rootFolder = nodePath.basename(nodePath.resolve());
 
 const buildFolder = `./dist`;
-const srcFolder   = `./assets`;
+const srcFolder = `./assets`;
 
 export const path = {
 	build: {
-		js: `${buildFolder} / js / `,
-		css: `${buildFolder} / css / `,
-		images: `${buildFolder} / img / `,
+		js: `${buildFolder}/js/`,
+		css: `${buildFolder}/css/`,
+		images: `${buildFolder}/img/`,
 		// fonts: `${buildFolder}/fonts/`,
 	},
 	src: {
-		js: `${srcFolder} / js`,
-		scss: `${srcFolder} / scss/*.scss`,
-		php: ['**/ * .php', ' ! vendor/**/ * .php'],
-		images: `${srcFolder} / img/**/ * .{jpg,jpeg,png,gif,webp}`,
-	svg: `${srcFolder} / img/**/ * .svg`,
+		js: `${srcFolder}/js`,
+		scss: `${srcFolder}/scss/*.scss`,
+		php: ['**/*.php', '!vendor/**/*.php'],
+		images: `${srcFolder}/img/**/*.{jpg,jpeg,png,gif,webp}`,
+		svg: `${srcFolder}/img/**/*.svg`,
 		// fonts: `${srcFolder}/fonts/*.*`,
 	},
 	watch: {
-		js: `${srcFolder} / js/**/ * .js`,
-		scss: `${srcFolder} / scss/**/ * .scss`,
+		js: `${srcFolder}/js/**/*.js`,
+		scss: `${srcFolder}/scss/**/*.scss`,
 		php: ['**/*.php', '!vendor/**/*.php'],
-		images: `${srcFolder} / img/**/ * .{jpg,jpeg,png,gif,webp,svg,ico}`,
+		images: `${srcFolder}/img/**/*.{jpg,jpeg,png,gif,webp,svg,ico}`,
 	},
 	clean: buildFolder,
 	buildFolder: buildFolder,
 	srcFolder: srcFolder,
 	rootFolder: rootFolder,
-	};
+};
