49a86004f34c4aa46f4b88c9b7d947feeeb0339f
Andrey-Sivak <andrey.sivak.dev@gmail.com>
2025-05-19 22:01:35 +0300

    chore: refactor SVG usage for card decor border

--- guten/src/blocks/home-statistics-showcase/render.php
+++ guten/src/blocks/home-statistics-showcase/render.php
@@ -20,19 +20,7 @@ if (empty($stats)) {
 		<?php foreach ($stats as $stat) : ?>
 			<div class="<?php echo esc_attr($base_class . '__item'); ?>">
 				<div class="<?php echo esc_attr($base_class . '__item-decor-bg'); ?>">
-					<svg preserveAspectRatio="none" viewBox="0 0 336 381" fill="none">
-						<path
-							d="M330 25C330 14.5066 321.493 6 311 6H214.704L219.262 0H311L311.646 0.0078125C324.94 0.344757 335.655 11.0602 335.992 24.3545L336 25V101.937L330 109.837V25ZM24.3545 380.992C10.8457 380.65 0 369.591 0 356V288.696L6 280.797V356C6 366.33 14.2429 374.734 24.5098 374.994L25 375H128.609L124.053 381H25L24.3545 380.992Z"
-							fill="url(#paint0_linear_22_659)"/>
-						<defs>
-							<linearGradient id="paint0_linear_22_659" x1="1.27078e-07" y1="190.5" x2="325.763"
-											y2="190.5" gradientUnits="userSpaceOnUse">
-								<stop stop-color="#007EA7"/>
-								<stop offset="1" stop-color="#00477A"/>
-							</linearGradient>
-						</defs>
-					</svg>
-
+					<?php get_template_part('/vector-images/card-decor-border'); ?>
 				</div>
 				<div class="<?php echo esc_attr($base_class . '__item-content'); ?>">
 					<div class="<?php echo esc_attr($base_class . '__number pm-section-heading'); ?>">
---
+++ vector-images/card-decor-border.php
@@ -0,0 +1,12 @@
+<svg preserveAspectRatio="none" viewBox="0 0 336 381" fill="none">
+	<path
+		d="M330 25C330 14.5066 321.493 6 311 6H214.704L219.262 0H311L311.646 0.0078125C324.94 0.344757 335.655 11.0602 335.992 24.3545L336 25V101.937L330 109.837V25ZM24.3545 380.992C10.8457 380.65 0 369.591 0 356V288.696L6 280.797V356C6 366.33 14.2429 374.734 24.5098 374.994L25 375H128.609L124.053 381H25L24.3545 380.992Z"
+		fill="url(#paint0_linear_22_659)"/>
+	<defs>
+		<linearGradient id="paint0_linear_22_659" x1="1.27078e-07" y1="190.5" x2="325.763"
+						y2="190.5" gradientUnits="userSpaceOnUse">
+			<stop stop-color="#007EA7"/>
+			<stop offset="1" stop-color="#00477A"/>
+		</linearGradient>
+	</defs>
+</svg>
