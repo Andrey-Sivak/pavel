b4b4aca99f6fce37ee09b884f8c4a64d74c0109c
Andrey-Sivak <andrey.sivak.dev@gmail.com>
2025-05-26 00:38:54 +0300

    chore: update responsive styles for navigation and header design

--- template-parts/logo.php
+++ template-parts/logo.php
@@ -7,6 +7,6 @@
 
 ?>
 
-<a href="<?php echo esc_attr( get_home_url() ); ?>" class="pm-logo font-bold xl:text-[31px] text-xl leading-[1.29]">
+<a href="<?php echo esc_attr( get_home_url() ); ?>" class="pm-logo font-bold 2xl:text-[31px] xl:text-[28px] text-xl leading-[1.29]">
 	Pavel Mikulenka
 </a>
