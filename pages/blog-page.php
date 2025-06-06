5620fc0b9db24b384fedc85d8acf6f2d6e45e353
Andrey-Sivak <andrey.sivak.dev@gmail.com>
2025-06-06 23:07:36 +0300

    feat(template): add new blog page template with basic structure

---
+++ pages/blog-page.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Template Name: Blog Page
+ *
+ * @package Pavel
+ */
+
+get_header();
+?>
+
+<main class="relative z-20 overflow-x-hidden">
+	<?php the_content(); ?>
+</main>
+
+<?php get_footer(); ?>
